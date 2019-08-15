7ca6a81fa9a279a29629a56f97fc7d77b9e1977d
nakamura196 <na.kamura.1263@gmail.com>
2019-08-15 10:35:27 +0900

    add batch

---
+++ batch.sh
@@ -0,0 +1,3 @@
+wget https://drive.google.com/uc?id=1BYPD6ZgYb-QuS2BYBhjG4hVX8lCADkFR -O classify_image_graph_def.pb
+wget https://drive.google.com/uc?id=1qnn08Zd0B_g3gHEXkKmz_hB0dNsuHLXB -O features.npy
+wget https://drive.google.com/uc?id=1JR6XPTkl-MW7ki4s7DoBaW8Ju2Shc9Qi -O data.json
